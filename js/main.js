bb7807d9ae0fb226d2caa932a56be75cb476c7a3
Stefano J. Attardi <stefano@attardi.org>
2010-03-28 17:21:57 -0400

    Live calls need not wait for domready.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 (function($, undefined) {
 
+    $('textarea').live('focus',  function (){
+        if ( $(this).parent().next().find('textarea').val() !== '' ) {
+            $('<li><textarea></textarea></li>')
+            .insertAfter( $(this).parent() );
+        }
+    });
+
+    $('textarea').live('keydown', function (event){
+        if (event.keyCode == '13') {
+            event.preventDefault();
+            $(this).parent().next().find('textarea').focus();
+        }
+
+        if (event.keyCode == '9') {
+            event.preventDefault();
+            $(this).parent().toggleClass('ihaveabullet');
+        }
+    });
+
     $(function(){
         $('<span/>') //just for fun
         .html('fox')
         .appendTo('h1');
 
         $('#imalist').sortable();
-
-        $('textarea').live('focus',  function (){
-            if ( $(this).parent().next().find('textarea').val() !== '' ) {
-                $('<li><textarea></textarea></li>')
-                .insertAfter( $(this).parent() );
-            }
-        });
-
-        $('textarea').live('keydown', function (event){
-            if (event.keyCode == '13') {
-                event.preventDefault();
-                $(this).parent().next().find('textarea').focus();
-            }
-
-            if (event.keyCode == '9') {
-                event.preventDefault();
-                $(this).parent().toggleClass('ihaveabullet');
-            }
-        });
     });
 
 })(jQuery);
