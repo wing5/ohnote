56d003d72a2585c5f4a9098df8e4532f49242159
Stefano J. Attardi <stefano@attardi.org>
2010-03-28 18:17:28 -0400

    Added delete row on backspace.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 (function($, undefined) {
 
     $('textarea').live('keydown', function (event){
-        if (event.keyCode == '13') { // enter
+        if (event.keyCode == 13) { // enter
             event.preventDefault();
             $('<li><textarea></textarea></li>')
             .insertAfter( $(this).parent() )
             .find('textarea').focus();
         }
 
-        if (event.keyCode == '27') { // esc
+        if (event.keyCode == 27) { // esc
             event.preventDefault();
             $(this).parent().toggleClass('ihaveabullet');
+        }
+
+        if (event.keyCode == 8) { // backspace
+            if ($(this).val() === '') {
+                event.preventDefault();
+                $(this).parent().prev().find('textarea').focus();
+                $(this).parent().remove();
+            }
         }
     });
 
